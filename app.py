9bdce03cf42feaed3879d2c7f6fc932a669f2c19
Elvin Cooper <ing.elvin01cooper@gmail.com>
2025-06-17 09:04:01 -0400

    fix: corriendo url con errores

--- app.py
+++ app.py
@@ -86,9 +86,10 @@ def create_app(testing=False):
 app = create_app(testing=False)
 
 if __name__ == '__main__':
-    port = int(os.getenv('FLASK_RUN_PORT', 5000))
-    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
-    app.run(host=host, port=port, debug=True)
+    app.run() 
+    # port = int(os.getenv('FLASK_RUN_PORT', 5000))
+    # host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+    # app.run(host=host, port=port, debug=True)
 
 
 
