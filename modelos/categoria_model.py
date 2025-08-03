4584e12f2cd83a0bc816ac1b05f1fd2106fd008d
Elvin cooper <ing.elvin01cooper@gmail.com>
2025-08-02 20:08:12 -0400

    Intentando corregir...

--- modelos/categoria_model.py
+++ modelos/categoria_model.py
@@ -6,6 +6,6 @@ class Categoria(db.Model):
 
     id_categoria = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
     descripcion = db.Column(db.String(50), nullable=False)
-    eslogan     = db.Column(db.String(100))
+    eslogan_cat     = db.Column(db.String(100))
     id_multimedia = db.Column(db.String, db.ForeignKey('multimedia.id_multimedia'), nullable=True)
     post = db.relationship('Post', back_populates='categoria', lazy=True)
